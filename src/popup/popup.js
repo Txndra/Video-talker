ba463139fde25471bb355aa120670cbf780b72da
Txndra <seandougan04@gmail.com>
2024-03-10 01:01:21 +0000

    feat: update styles, js and html

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,10 +1,7 @@
-// popup.js
-
 // Function to initialize the typewriter effect on the specified element
 function initializeTypewriter(elementId, message) {
-  // Typewriter effect function
   function typewriterEffect(text, element) {
-    const speed = 50; // Adjust the typing speed (milliseconds per character)
+    const speed = 50;
     let i = 0;
 
     function type() {
@@ -18,20 +15,13 @@ function initializeTypewriter(elementId, message) {
     type();
   }
 
-  // Get the element by ID
   const element = document.getElementById(elementId);
 
-  // Check if the element exists
   if (element) {
-    // Clear any existing content in the element
     element.innerHTML = "";
 
-    // Start the typewriter effect with the specified message
     typewriterEffect(message, element);
   } else {
     console.error(`Element with ID ${elementId} not found.`);
   }
 }
-
-// Example usage
-initializeTypewriter("writeMessage", "Write your message below!");
