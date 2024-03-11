284270caf6fecaf90ab65e096380846f788147f4
Txndra <seandougan04@gmail.com>
2024-03-11 17:49:24 +0000

    fix: reset js files

--- src/background/background.js
+++ src/background/background.js
@@ -1,13 +1,2 @@
 // for debugging
 console.log("background.js loaded");
-
-// Listen for messages from the content script
-chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-  // Check if the message contains the videoId property
-  if (message.videoId) {
-    var videoId = message.videoId;
-    // Do something with the videoId
-    console.log("Received videoId: " + videoId);
-    // You can perform any further processing or actions with the videoId here
-  }
-});
--- src/content/content.js
+++ src/content/content.js
@@ -1,15 +1,1 @@
-// Check if the current page is a YouTube video page
-if (window.location.href.includes("youtube.com/watch")) {
-  // Extract the video ID from the URL
-  const videoId = new URLSearchParams(window.location.search).get("v");
-
-  console.log("Current page is a YouTube video page.");
-  console.log("Video ID:", videoId);
-
-  // Send a message to the background script with the video ID
-  chrome.runtime.sendMessage({ videoId: videoId }, (response) => {
-    console.log("Message sent to background script. Response:", response);
-  });
-} else {
-  console.log("Not a YouTube video page.");
-}
+console.log("content.js loaded");
