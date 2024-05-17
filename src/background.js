dc612eeefbc582dda48c776ef8ae15e2ebad5fb6
Txndra <seandougan04@gmail.com>
2024-05-17 11:21:07 +0100

    feat: extract video title and description

--- src/background.js
+++ src/background.js
@@ -1,11 +1,16 @@
 // for debugging
 console.log("background.js loaded");
 
-chrome.tabs.executeScript({
-  file: "content.js",
-});
-
 // Listen for messages from the popup
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
   console.log("Message received in background.js");
+  if (message.action === "videoInfo") {
+    // Received video info from content script
+    const videoTitle = message.title;
+    const videoDescription = message.description;
+
+    // Log the video info (you can perform further processing here)
+    console.log("Video Title:", videoTitle);
+    console.log("Video Description:", videoDescription);
+  }
 });
